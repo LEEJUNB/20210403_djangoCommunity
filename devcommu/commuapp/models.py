77130c1a4cf5cd35174a5b3ab8b96670fb708fc0
LEEJUNB <topguide82@gmail.com>
2021-03-24 15:06:43 +0900

    DB init

--- devcommu/commuapp/models.py
+++ devcommu/commuapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+# 게시물 모음
+class Post(models.Model) : 
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
