2ffb390fff02c51d9284e624a7520c39d0ee0e81
LEEJUNB <topguide82@gmail.com>
2021-03-27 13:13:59 +0900

    글쓰기 버튼 기능 init

---
+++ devcommu/commuapp/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .model import Post # 모델기반이므로
+
+# forms의 ModelForm을 상속받아 만듦
+class PostForm(forms.ModelForm):
+    class Meta : 
+        model = Post
+        fields = '__all__'
--- devcommu/commuapp/views.py
+++ devcommu/commuapp/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .forms import PostForm # forms.py의 PostForm객체 불러오기
 
 # Create your views here.
 def home(request) :
     return render(request, 'index.html')
+
+def postcreate(request) : 
+    # request method가 POST
+    if request.method == 'POST' :
+        pass
+        # 입력값 저장
+
+    # request method가 GET
+    # form 입력 html 띄우기
+    else : 
+        form = PostForm() # forms.py의 PostForm객체클래스
+    return render(request, 'post_form.html', {'form':form})
+
+    
