7d2c6139c4ffc20640c580873daba7212ba656c9
LEEJUNB <topguide82@gmail.com>
2021-03-27 13:37:17 +0900

    작성후 새글버튼 클릭시 POST요청 동작

--- devcommu/commuapp/models.py
+++ devcommu/commuapp/models.py
@@ -6,3 +6,7 @@ class Post(models.Model) :
     title = models.CharField(max_length=200)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    # 게시글 작성시 DB에 title이 나오도록함
+    def __str__(self) :
+        return self.title
--- devcommu/commuapp/views.py
+++ devcommu/commuapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import PostForm # forms.py의 PostForm객체 불러오기
 
 # Create your views here.
@@ -8,13 +8,14 @@ def home(request) :
 def postcreate(request) : 
     # request method가 POST
     if request.method == 'POST' :
-        pass
         # 입력값 저장
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid() :
+            form.save()
+            return redirect('home') # 저장후 home으로 url이동
 
     # request method가 GET
     # form 입력 html 띄우기
     else : 
         form = PostForm() # forms.py의 PostForm객체클래스
     return render(request, 'post_form.html', {'form':form})
-
-    
