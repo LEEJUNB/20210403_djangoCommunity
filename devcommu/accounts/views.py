07d824820b24c3b1aa0656368c82356c0350969e
LEEJUNB <topguide82@gmail.com>
2021-04-01 12:23:58 +0900

    로그인 함수

--- devcommu/accounts/views.py
+++ devcommu/accounts/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth # login,out을 위해 필수
 
 def login(request) : 
     # request == POST
-    if request.method == "POST" : 
-        pass
     # Login
+    if request.method == "POST" : 
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = auth.authenticate(request, username=username, password=password)
+    
+        # 실제 DB에 있는 회원이라면 로그인 진행
+        if user is not None : 
+            auth.login(request, user)
+            return redirect('home')
+        # 회원이 아니라면
+        else :
+            return render(request, 'bad_login.html')
 
     # request == GET
     else : 
