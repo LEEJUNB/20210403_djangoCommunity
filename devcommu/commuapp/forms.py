f6a52013528eae4a514231f387e90e802d232fac
LEEJUNB <topguide82@gmail.com>
2021-04-03 18:16:59 +0900

    새글, 댓글 폼 수정

--- devcommu/commuapp/forms.py
+++ devcommu/commuapp/forms.py
@@ -7,9 +7,38 @@ class PostForm(forms.ModelForm):
     class Meta : 
         model = Post # models.py의 Post객체
         fields = '__all__'
+    
+    # HTML에 속성을 넣고 싶다면 init으로 가능하다
+    # Bootstrap클래스를 wiget으로 지정
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+
+        self.fields['title'].widget.attrs = {
+            'class': 'form-control',
+            'placeholder': "글 제목을 입력해주세요",
+            'rows': 20
+        }
+
+        self.fields['body'].widget.attrs = {
+            'class': 'form-control',
+            'placeholder': "글 제목을 입력해주세요",
+            'rows': 20,
+            'cols' : 100
+        }
 
 # 댓글작성
 class CommentForm(forms.ModelForm):
     class Meta : 
         model = Comment # models.py의 Comment 객체
         fields = ['comment']
+        
+    # HTML에 속성을 넣고 싶다면 init으로 가능하다
+    # Bootstrap클래스를 wiget으로 지정
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+
+        self.fields['comment'].widget.attrs = {
+            'class': 'form-control',
+            'placeholder': "댓글을 입력해주세요",
+            'rows': 10
+        }
