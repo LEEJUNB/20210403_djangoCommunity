e457bc57b508646775f9e99309a39f1bc83f32fd
LEEJUNB <topguide82@gmail.com>
2021-04-04 11:27:34 +0900

    자유게시판 관련 함수

--- devcommu/commuapp/views.py
+++ devcommu/commuapp/views.py
@@ -42,3 +42,39 @@ def new_comment(request, post_id) :
         # 저장한다.
         finished_form.save()
     return redirect('detail', post_id) # 댓글작성한 상세페이지로 이동
+
+## 자유게시판 관련
+def freehome(request):
+    # posts = Post.objects.all()
+    # FreePost객체를 모조리 띄워라
+    freeposts = FreePost.objects.filter().order_by('-date')
+    return render(request, 'free_index.html', {'freeposts': freeposts})
+
+
+def freepostcreate(request):
+    if request.method == 'POST' or request.method == 'FILES':
+        
+        form = FreePostForm(request.POST, request.FILES)
+        if form.is_valid():
+            unfinished = form.save(commit=False)
+            unfinished.author = request.user            # user 추가!
+            unfinished.save()
+            return redirect('freehome')
+    else:
+        form = FreePostForm()
+    return render(request, 'free_post_form.html', {'form':form})
+
+
+def freedetail(request, post_id):
+    post_detail = get_object_or_404(FreePost, pk=post_id)
+    comment_form = FreeCommentForm()
+    return render(request, 'free_detail.html', {'post_detail':post_detail, 'comment_form': comment_form})
+
+
+def new_freecomment(request, post_id):
+    filled_form = FreeCommentForm(request.POST)
+    if filled_form.is_valid():
+        finished_form = filled_form.save(commit=False)
+        finished_form.post = get_object_or_404(FreePost, pk=post_id)
+        finished_form.save()
+    return redirect('freedetail', post_id)
